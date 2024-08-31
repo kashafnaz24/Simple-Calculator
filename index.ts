9612a1bde961ab5d5d4d00b26fd527ba31591d73
Kashaf Naz <kashafnazk389@gmail.com>
2024-08-31 17:06:44 +0500

    A calculator with Typescript, Node.js and inquirer

---
+++ index.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+
+
+  const answers = await inquirer.prompt([
+    {
+      message: "Enter first number",
+      type: "number",
+      name: "firstNumber"
+    },
+    {
+      message: "Enter second number",
+      type: "number",
+      name: "secondNumber"
+    },
+    {
+        message: "selecte one of the operator to perform action",
+        type:"list",
+        name:"operator",
+        choices:["Addition","Subtraction","Multiplication","Division"]
+    }
+  ]);
+  
+  // Condition statement
+  if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+  }
+   else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+  }
+   else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+  }
+   else if (answers.operator === "Division") {
+    if (answers.secondNumber !== 0) {
+      console.log(answers.firstNumber / answers.secondNumber);
+    } else {
+      console.log("Cannot divide by zero");
+    }
+  }
+  
+
+
